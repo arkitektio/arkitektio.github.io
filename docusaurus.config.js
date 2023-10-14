c4ca5d50b1932707d6be53c7c92381068c99237f
jhnnsrs <jhnnsrs@gmail.com>
2023-10-14 12:19:39 +0200

    tet again

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,8 +44,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "jhnnsrs", // Usually your GitHub org/user name.
-  projectName: "doks", // Usually your repo name.
+  organizationName: "arkitektio", // Usually your GitHub org/user name.
+  projectName: "arkitektio.github.io", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
