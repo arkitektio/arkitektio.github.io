a301577d383670c57b3850281c43ee87ad2eae14
jhnnsrs <jhnnsrs@users.noreply.github.com>
2023-10-24 21:51:38 +0000

    deploy: 95a29ae483fb60804e2f2104cad38c5897192572

--- assets/js/ec8aba26.f6503097.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdoks=self.webpackChunkdoks||[]).push([[5749],{15156:e=>{e.exports=JSON.parse('{"title":"Companion Apps","slug":"/category/companion-apps","permalink":"/docs/category/companion-apps","navigation":{"previous":{"title":"mikro_next","permalink":"/docs/services/next/mikro_next"},"next":{"title":"MikroJ","permalink":"/docs/apps/mikroj"}}}')}}]);
