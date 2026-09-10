3d5a420e5cf919c1cc1c1934c98f8d8173c9377a
jhnnsrs <jhnnsrs@gmail.com>
2026-09-10 16:11:42 +0200

    fix: imagej is now plugin

--- src/components/docs/fiji-grid.tsx
+++ src/components/docs/fiji-grid.tsx
@@ -1,40 +1,32 @@
 // @ts-nocheck
 import React from "react";
-import { BsWindows } from "react-icons/bs";
-import { FaApple, FaLinux } from "react-icons/fa";
+import { FaFileArchive, FaJava } from "react-icons/fa";
+
+const RELEASES =
+  "https://github.com/arkitektio/imagej-arkitekt-plugin/releases/latest";
 
 export const FijiGrid = ({ children }) => {
   return (
-    <div className="grid grid-cols-3 md:grid-cols-3 gap-4 mb-3 ">
+    <div className="grid grid-cols-2 md:grid-cols-2 gap-4 mb-3 ">
       <a
-        target="_blank"
-        className="p-2 bg-gray-500 rounded-sm px-3 py-2 rounded-lg text-white cursor-not-allowed hover:bg-gray-500 hover:text-white my-auto flex flex-col items-center my-auto "
-      >
-        <div className="my-auto mr-2">
-          <FaLinux size={"3em"} />
-        </div>{" "}
-        <div className="my-auto">Linux</div>
-      </a>
-      <a
-        href={
-          "https://github.com/arkitektio-apps/mikroj/releases/latest/download/MikroJApp.zip"
-        }
+        href={RELEASES}
         target="_blank"
         className="bg-primary-300 rounded-sm px-3 py-2 rounded-lg text-white cursor-pointer hover:bg-primary-500 hover:text-white flex flex-col items-center my-auto"
       >
         <div className="my-auto mr-2">
-          <BsWindows size={"3em"} />
+          <FaJava size={"3em"} />
         </div>{" "}
-        <div className="my-auto">Windows</div>
+        <div className="my-auto">Jar (all platforms)</div>
       </a>
       <a
+        href={RELEASES}
         target="_blank"
-        className="bg-gray-500 rounded-sm px-3 py-2 rounded-lg text-white cursor-not-allowed hover:bg-gray-500 hover:text-white my-auto flex flex-col items-center my-auto"
+        className="bg-primary-300 rounded-sm px-3 py-2 rounded-lg text-white cursor-pointer hover:bg-primary-500 hover:text-white flex flex-col items-center my-auto"
       >
         <div className="my-auto mr-2">
-          <FaApple size={"3em"} />
+          <FaFileArchive size={"3em"} />
         </div>{" "}
-        <div className="my-auto">Mac</div>
+        <div className="my-auto">Zip (all platforms)</div>
       </a>
     </div>
   );
